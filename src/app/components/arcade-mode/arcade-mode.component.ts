9ab5619c6900e63311af505bf28a821b094a1086
Francisco Noguera Fuentes <francisco.noguera.fuentes@hotmail.com>
2018-06-05 11:46:43 +0200

    Añadidas nuevas funcionalidades

--- src/app/components/arcade-mode/arcade-mode.component.ts
+++ src/app/components/arcade-mode/arcade-mode.component.ts
@@ -3,6 +3,9 @@ import { Card } from './../../interfaces/Card';
 import { ApiConnectionService } from './../../services/ApiConnection/api-connection.service';
 import { Component, OnInit } from '@angular/core';
 
+//Falta por definir la mecánia de puntos
+//Falta el fin del juego
+//Hay un error en el cronometro en modo survival
 
 @Component({
   selector: 'app-arcade-mode',
@@ -23,6 +26,7 @@ export class ArcadeModeComponent implements OnInit {
   //Variables de la tarjeta
   private cards: Card[] = [];
   private positionCard: number = 0;
+  private gameTime: number;
   public gameImage: string;
   public gameClue: string;
   private gameSolution: string;
@@ -35,12 +39,14 @@ export class ArcadeModeComponent implements OnInit {
   //Variables de la pista
   public displayClue: boolean = false;
 
+  //Variables del nivel de difuminado
+  public blurLevel: number = 0;
+
   constructor(private apiConnectionService: ApiConnectionService) {
-    //this.gameMode = localStorage.getItem('gameMode');
 
     //Tengo que modificarlo para
-    this.segundos = 60;
-    this.isPaused = false;
+    //this.segundos = 60;
+    //this.isPaused = false;
     setInterval(() => this.tick(), 1000);
   }
 
@@ -66,6 +72,7 @@ export class ArcadeModeComponent implements OnInit {
     */
     var success: boolean = false;
     while((success == false) && (this.positionCard < this.cards.length)){
+      this.blurLevel = 0;
       success = this.loadCard();
       this.positionCard++;
     }
@@ -87,15 +94,36 @@ export class ArcadeModeComponent implements OnInit {
       this.arrayKeyboard = this.shuffleArray(this.cards[this.positionCard].letters);
 
       if(this.gameMode == "Arcade"){
+        this.gameTime = 60;
         this.segundos = 60;
         this.isPaused = false;
       } else if (this.gameMode == "Party"){
+        this.gameTime = this.cards[this.positionCard].time;
         this.segundos = this.cards[this.positionCard].time;
+        this.isPaused = false;
+      } else if((this.positionCard == 0) && (this.gameMode == "Survival")){
+        this.gameTime = 300;
+        this.segundos = 300;
         this.isPaused = false;
       }
       return true;
     } else {
       return false;
+    }
+  }
+
+  isRightAnwser(){
+
+    var aux: string;
+    for (var i=0; i<this.gameSolution.length; i++){
+      if(i==0){
+        aux = this.arraySolution[i];
+      } else {
+        aux += this.arraySolution[i];
+      }
+    }
+    if(aux == this.gameSolution){
+      this.loadNextCard();
     }
   }
 
@@ -107,6 +135,7 @@ export class ArcadeModeComponent implements OnInit {
       let aux: string = this.arraySolution[position];
       this.arraySolution[position] = this.keyboardAux;
       this.keyboardAux = aux;
+      this.isRightAnwser();
     } else if(keyboard == "keyboard"){
       let aux: string = this.arrayKeyboard[position];
       this.arrayKeyboard[position] = this.keyboardAux;
@@ -140,7 +169,28 @@ export class ArcadeModeComponent implements OnInit {
     if(this.isPaused == false){
       if(this.segundos > 0){
         --this.segundos
-      } else{ this.togglePause }
+
+        if((this.segundos <= this.gameTime) && (this.segundos > this.gameTime*4/5)){
+          this.blurLevel = 0;
+        } else if((this.segundos <= this.gameTime*4/5) && (this.segundos > this.gameTime*3/5)){
+          this.blurLevel = 1;
+        } else if((this.segundos <= this.gameTime*3/5) && (this.segundos > this.gameTime*2/5)){
+          this.blurLevel = 2;
+        } else if((this.segundos <= this.gameTime*2/5) && (this.segundos > this.gameTime*1/5)){
+          this.blurLevel = 3;
+        }else if(this.segundos <= this.gameTime*1/5){
+          this.blurLevel = 4;
+        }
+
+
+      } else{
+        this.togglePause();
+        if(this.gameMode == "Survival"){
+          this.endGame();
+        } else if ((this.gameMode == "Arcade") ||(this.gameMode == "Party")){
+          this.loadNextCard();
+        }
+      }
 
     }
   }
